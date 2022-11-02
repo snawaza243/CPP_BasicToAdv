2ec90644eafccf4bad33f4a720d0eb8c0929e58c
Shahnawaz Alam <snawaza243@gmail.com>
2022-11-02 07:59:24 +0530

    ReactRouterComponent

--- react-practice-sam/src/index.js
+++ react-practice-sam/src/index.js
@@ -1,17 +1,22 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import Home from './Router/Home'
-import Blogs from './Router/Blogs'
-import Contact from './Router/Contact'
-import NoPage from './Router/NoPage'
-
+import Home from './ReactRouter/Home'
+import Blogs from './ReactRouter/Blogs'
+import Contact from './ReactRouter/Contact'
+import NoPage from './ReactRouter/NoPage'
+import { BrowserRouter,Route,Routes } from 'react-router-dom'
+import Layout from './ReactRouter/Layout'
 
 const root = ReactDOM.createRoot(document.getElementById('root'))
 root.render(
-  <>
-    <Home />
-    <Blogs />
-    <Contact />
-    <NoPage />
-  </>
+  <BrowserRouter>
+    <Routes>
+      <Route path='/' element={<Layout />}>
+        <Route index element={<Home />} />
+        <Route path='blogs' element={<Blogs />} />
+        <Route path='contact' element={<Contact />} />
+        <Route path='*' element={<NoPage />} />
+      </Route>
+    </Routes>
+  </BrowserRouter>
 )
