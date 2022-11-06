170e95e7c1a90e6575905ed7821cb8600143ff2f
Shahnawaz Alam <snawaza243@gmail.com>
2022-11-06 08:58:36 +0530

    Reacat index update

--- React Practice Sam/src/index.js
+++ React Practice Sam/src/index.js
@@ -1,6 +1,6 @@
 import ReactDOM from 'react-dom/client'
 import React from 'react'
-import A from './App/App'
+import A from './ToDoApp/ToDoApp'
 
 const root = ReactDOM.createRoot(document.getElementById('root'))
 root.render(
